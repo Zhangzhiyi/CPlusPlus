43d22134cdb48557c66be55bca04893f1f923982
zhangzhiyi <qq361119908@gmail.com>
2013-05-31 11:03:45 +0800

    static_cast运算法

--- HelloWorld/TestRTTI/Classes/TestRTTI.cpp
+++ HelloWorld/TestRTTI/Classes/TestRTTI.cpp
@@ -36,10 +36,7 @@ public:
 };
 
 /**不包含虚函数**/
-class Base 
-{
-
-};
+class Base {};
 class Derived: public Base {};
 
 
@@ -63,28 +60,31 @@ Grand * getOne(void)
 }
 int _tmain(int argc, _TCHAR* argv[])
 {
-	srand(time(0));
-	Grand * pg;
-	Superb * ps;
-	for (int i = 0; i <5; i++)
 	{
-		pg = getOne();
-		cout << "Now Processing type " << typeid(*pg).name() <<".!\n";
-		pg->Speak();
-		//dynamic_cast运算符只能用于向上转型，向下转型会返回空指针
-		if (ps = dynamic_cast<Superb*>(pg))
+		srand(time(0));
+		Grand * pg;
+		Superb * ps;
+		for (int i = 0; i <5; i++)
 		{
-			ps->Say();
-		}
-		else{
-			cout << typeid(*pg).name() << " dynamic_cast  Superb failed!!\n";
-		}
-		/**typeid运算符使得能够确定两个对象是否为同种类型。typeid运算符返回一个对type_info对象的引用**/
-		if (typeid(Magnificent) == typeid(*pg))
-		{
-			cout << "Yes, You ' re really magnificent.\n";
+			pg = getOne();
+			cout << "Now Processing type " << typeid(*pg).name() <<".!\n";
+			pg->Speak();
+			//dynamic_cast运算符只能用于向上转型，向下转型会返回空指针
+			if (ps = dynamic_cast<Superb*>(pg))
+			{
+				ps->Say();
+			}
+			else{
+				cout << typeid(*pg).name() << " dynamic_cast  Superb failed!!\n";
+			}
+			/**typeid运算符使得能够确定两个对象是否为同种类型。typeid运算符返回一个对type_info对象的引用**/
+			if (typeid(Magnificent) == typeid(*pg))
+			{
+				cout << "Yes, You ' re really magnificent.\n";
+			}
 		}
 	}
+	
 	/*如果表达式的类型是类类型且至少包含有一个虚函数，则typeid操作符返回表达式的动态类型，
 	需要在运行时计算；否则，typeid操作符返回表达式的静态类型，在编译时就可以计算
 	
@@ -105,21 +105,49 @@ int _tmain(int argc, _TCHAR* argv[])
 		dynamic_cast将引发类型为bad_cast的异常，这种异常时从exception类派生而来的，它是在头文件typeinfo中定义的。
 	**/
 	cout << "----------测试dynamic_cast用于引用的转换----------\n";
-	Magnificent magnificent;
-	Grand & rgrand = magnificent;
-	cout << typeid(rgrand).name() << endl;
-	Magnificent rmagnificent = dynamic_cast<Magnificent&>(rgrand);
-	cout << typeid(rmagnificent).name() << endl;
-
-	Grand grand;
-	Grand & rg = grand;
-	try
 	{
-		//不可以向下转型，引用转型抛出异常
-		Superb & rs = dynamic_cast<Superb &>(rg);
+		Magnificent magnificent;
+		Grand & rgrand = magnificent;
+		cout << typeid(rgrand).name() << endl;
+		Magnificent rmagnificent = dynamic_cast<Magnificent&>(rgrand);
+		cout << typeid(rmagnificent).name() << endl;
+
+		Grand grand;
+		Grand & rg = grand;
+		try
+		{
+			//不可以向下转型，引用转型抛出异常
+			Superb & rs = dynamic_cast<Superb &>(rg);
+		}
+		catch(bad_cast & bc){
+			cout << "bad_cast" << endl;
+		}
 	}
-	catch(bad_cast & bc){
-		cout << "bad_cast" << endl;
+	
+
+	{
+		cout << "----------测试static_cast的转换----------\n";
+		/*
+			static_cast向上转型或者向下转型都是合法的，而转换无关类型编译期就会报错。可以用static_cast将整形转换为枚举，double转换int，
+			将float转换为long以及其他各种类型转换
+		*/
+
+		Grand grand;
+		Superb superb;
+		Magnificent magnificent;
+		
+		//向下转型
+		Magnificent * pm = static_cast<Magnificent*>(&grand);
+		pm->Speak(); //"I am a Superb class
+		//pm->Say();  调用Say方法出错，因为里面有调用到Magnificent的特有的变量，需要this指针
+		
+		//向上转型
+		Superb * ps = static_cast<Superb*>(&magnificent);
+		ps->Speak(); //I am a Magnificent class
+		ps->Say();  // I hold the character  A and the integer 0 !
+
+		//无关类型不可以转换
+		//Base* pb = static_cast<Base*>(&grand); //error C2440: “static_cast”: 无法从“Grand *”转换为“Base *”
 	}
 
 
@@ -135,8 +163,10 @@ int _tmain(int argc, _TCHAR* argv[])
 	
 
 	void* vc = "1234";
-	char* data = reinterpret_cast<char*>(vc);
+	char* data = static_cast<char*>(vc);
 	cout << data <<endl;
+	char* data2 = (char*)vc;
+	cout << data2 <<endl;
 	system("Pause");
 	return 0;
 }
